103e606ed4c68c054580cb6a44ddc9c6b2194630
Lokesh Mittal <lokesh.adonis@gmail.com>
2026-02-25 15:41:35 -0500

    Testing POST /actuator/refresh endpoint for configuration refresh for accounts, loans and cards micro service

--- cards/src/main/java/com/lokesh/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/lokesh/cards/dto/CardsContactInfoDto.java
@@ -1,10 +1,16 @@
 package com.lokesh.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String, String> contacts, List<String> phones) {
+@Getter @Setter
+public class CardsContactInfoDto {
+    private String message;
+    private Map<String, String> contacts;
+    private List<String> phones;
 }
