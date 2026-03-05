33a11757d9210e2e18e6689a82ec55ccc10679af
Lokesh Mittal <lokesh.adonis@gmail.com>
2026-03-05 15:03:21 -0500

    Registering Accounts micro service at Eureka Server

--- accounts/src/main/java/com/lokesh/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/lokesh/accounts/AccountsApplication.java
@@ -3,11 +3,13 @@ package com.lokesh.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 @ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class AccountsApplication {
 
 	public static void main(String[] args) {
