46c20eabccfdc91fe8d015934ab7bc8db19618a3
Lokesh Mittal <lokesh.adonis@gmail.com>
2026-03-05 20:16:23 -0500

    Registering Loans and Cards micro service at Eureka Server

--- accounts/src/main/java/com/lokesh/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/lokesh/accounts/AccountsApplication.java
@@ -3,13 +3,11 @@ package com.lokesh.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 @ConfigurationPropertiesScan
-@EnableDiscoveryClient
 public class AccountsApplication {
 
 	public static void main(String[] args) {
